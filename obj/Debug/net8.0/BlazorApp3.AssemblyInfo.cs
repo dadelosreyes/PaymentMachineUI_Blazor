0d7260f4d6f1654a1147add0f4d31e4eb17df441
Hujeeee <jehurecaido582@gmail.com>
2024-10-30 07:41:24 +0800

    push

--- obj/Debug/net8.0/BlazorApp3.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a5a4a46e5148c52fed3dd9e4cea68eec63813d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95baa573dbcdfa9a5c926b143cd04aeb27136c6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
