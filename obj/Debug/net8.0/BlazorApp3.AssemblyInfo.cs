288bc94b75f24d3024940f82adf11865b5374a9d
77616104+dadelosreyes@users.noreply.github.com <77616104+dadelosreyes@users.noreply.github.com>
2024-11-05 09:14:57 +0800

    minor tweaks and fixed ignore files

--- obj/Debug/net8.0/BlazorApp3.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91310b854cb8cc1290673ac208d73e82e6f8f755")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e71a19e254d39bd557e8791e71cbb21d1a426312")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
